2b2f11ca4a7bdf2a021360a9b3ad123f5b2815d8
Nivekithan S <nivekithan363@gmail.com>
2022-05-21 22:16:26 +0530

    Add tweet table

--- supabase-local/supabase/seed.sql
+++ supabase-local/supabase/seed.sql
@@ -1,4 +1,22 @@
 
 INSERT INTO users (user_name, password_hash) 
 VALUES
-    ('demo', '$2a$10$x.KihysnxHvKds0SYXNkkuLLQKDaqaBsXYFM0UjosTUX.AQehnzOG') -- password is demo
+    ('demo', '$2a$10$x.KihysnxHvKds0SYXNkkuLLQKDaqaBsXYFM0UjosTUX.AQehnzOG'); -- password is demo
+
+
+
+
+WITH demo_user_id as (
+    SELECT
+        user_id AS id
+    FROM
+        users
+    WHERE 
+        user_name = 'demo'
+)
+
+
+INSERT INTO tweets(message, user_id)
+VALUES 
+    ('first tweet 🥳', (SELECT id FROM demo_user_id)),
+    ('second tweet 🥳', (SELECT id FROM demo_user_id))
